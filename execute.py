21973ebb5695d511a952c88228cea4846c5e7227
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-04-25 06:03:07 +0300

    adds find and account and confirm if account exist function to execute.py

--- execute.py
+++ execute.py
@@ -9,13 +9,25 @@ def create_account(account_type,user_name,password):
 
 def save_account(account):
     '''
-    Function to save contact
+    Function to save account
     '''
     account.save_account()
 
 def del_account(account):
     '''
-    Function to delete a contact
+    Function to delete an account
     '''
     account.delete_account()
 
+def find_account(account_type):
+    '''
+    Function that finds a account by account_type and returns the account
+    '''
+    return Account.find_by_account_type(account_type)
+
+
+def check_existing_account(account_type):
+    '''
+    Function that check if a account exists with the account_type and return a Boolean
+    '''
+    return Account.account_exist(account_type)
