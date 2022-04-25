1feaf09171ac225f29a9c0617cfb3475f7a35c89
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-04-25 05:56:15 +0300

    Adds save account function to execute.py

--- execute.py
+++ execute.py
@@ -6,3 +6,10 @@ def create_account(account_type,user_name,password):
     '''
     new_account = Account(account_type,user_name,password)
     return new_account
+
+def save_account(account):
+    '''
+    Function to save contact
+    '''
+    account.save_account()
+
